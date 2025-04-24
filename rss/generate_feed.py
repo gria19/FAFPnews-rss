c587af0326983634c75071020013ef7fc252ab6d
gria19 <zhangjinxin19@gmail.com>
2025-04-24 15:49:56 +0800

    Update generate_feed.py

--- rss/generate_feed.py
+++ rss/generate_feed.py
@@ -1,18 +1,20 @@
 from feedgen.feed import FeedGenerator
 from datetime import datetime
 
-
 def generate_rss(feed_items, output_file="rss.xml"):
     fg = FeedGenerator()
+    fg.id("https://foreignpolicy.com/")
     fg.title("Foreign News Digest")
-    fg.link(href="https://www.foreignaffairs.com", rel="alternate")
+    fg.link(href="https://foreignpolicy.com", rel="alternate")
     fg.description("Latest summarized news from Foreign Policy and Foreign Affairs")
+    fg.language("zh")
 
     for item in feed_items:
         fe = fg.add_entry()
+        fe.id(item["url"])
         fe.title(item["title"])
         fe.link(href=item["url"])
         fe.description(item["summary"])
         fe.pubDate(datetime.utcnow())
 
-    fg.rss_file(output_file)
+    fg.rss_file(output_file, pretty=True)
